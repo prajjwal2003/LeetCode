42ab99873ea082aa227f6aa3dfc8abd708471680
Prajjwal Gupta <112173450+prajjwal2003@users.noreply.github.com>
2023-05-08 11:13:59 +0530

    1572. Matrix Diagonal Sum

---
+++ diagonalSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int ans = 0;
+        int n = mat.size();
+        for(int i=0; i<n; i++){
+            ans+= mat[i][i];
+            ans+= mat[i][n-1-i];
+        }
+        if(n%2==0){
+            return ans;
+        }
+        ans-= mat[n/2][n/2];
+        return ans;
+    }
+};
