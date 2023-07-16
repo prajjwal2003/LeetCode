7ef93830ee255b4839d84b6bf34e81640d103aa1
Prajjwal Gupta <112173450+prajjwal2003@users.noreply.github.com>
2023-07-17 02:08:32 +0530

    sumOfSquares.cpp
    2778. Sum of Squares of Special Elements

---
+++ sumOfSquares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        for(int i=0; i<nums.size(); i++){
+            if(n%(i+1) == 0){
+                ans+= nums[i] * nums[i];
+            }
+        }
+        return ans;
+    }
+};
